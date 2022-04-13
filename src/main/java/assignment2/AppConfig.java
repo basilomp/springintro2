12a31f648807c3b6b205ecb6eaa54cf7edb9f341
basilomp <73391933+basilomp@users.noreply.github.com>
2022-04-13 23:17:21 +0300

    Delete src/main/java/assignment2 directory

--- src/main/java/assignment2/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package assignment2;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("assignment2")
-public class AppConfig {}
-
